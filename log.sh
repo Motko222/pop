aa186d02ab3e0513b66c7ba146d4283909488d60
Motko <77466238+Motko222@users.noreply.github.com>
2025-05-19 10:26:52 +0200

    Update log.sh

--- log.sh
+++ log.sh
@@ -3,4 +3,4 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) 
 folder=$(echo $path | awk -F/ '{print $NF}')
 
-journalctl -n 200 -u $folder.service -f --no-hostname -o cat
+docker logs -f popnode
